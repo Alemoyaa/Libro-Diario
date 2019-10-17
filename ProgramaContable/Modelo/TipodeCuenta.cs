ebd271b22204cbadfee752f725d079002dad64ac
Alejandro Moya <47389285+Alemoyaa@users.noreply.github.com>
2019-10-17 16:22:12 -0300

    Add files via upload

---
+++ ProgramaContable/Modelo/TipodeCuenta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaContable.Modelo
+{
+    class TipodeCuenta
+    {
+        private int id;
+        private string descripcionTipo;
+
+        public string DescripcionTipo { get => descripcionTipo; set => descripcionTipo = value; }
+        public int Id { get => id; set => id = value; }
+
+        public TipodeCuenta(int id, string descripcion)
+        {
+            Id = id;
+            DescripcionTipo = descripcion;
+        }
+    }
+
+
+
+}
